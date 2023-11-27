c3a44f45396bfca838d9fdf0b2f4af3ce305d1a7
Jumanov Alisher <zumanovaliser814@gmail.com>
2023-11-27 18:51:38 +0600

    update last port

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ export const buildServer = () => {
 				"http://127.0.0.1:3000",
 				"http://127.0.0.1:5000",
 				"http://localhost:5173",
-				"https://portfolio-tailwind-tan.vercel.app/"
+				"https://portfolio-tailwind-tan.vercel.app"
 			],
 			credentials: true
 		})
